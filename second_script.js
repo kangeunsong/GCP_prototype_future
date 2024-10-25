968d1ce6ea017429f0574d001c0ae9cb3ef3fc94
kangeunsong <dmxthd@naver.com>
2024-10-26 08:09:49 +0900

    rotate automatically

--- second_script.js
+++ second_script.js
@@ -1,9 +1,9 @@
+window.onload = function() {
+    setTimeout(spin, 2000);
+};
+
 function spin() {
     const roulette = document.getElementById('roulette');
     const randomDegree = Math.floor(Math.random() * 360) + 720;
     roulette.style.transform = `rotate(${randomDegree}deg)`;
-}
-
-function navigateTo(page) {
-    window.location.href = page;
 }
